24aee8de15cea0b43480430e31f47e3b9110196a
Ryan-Giard <114446023+Ryan-Giard@users.noreply.github.com>
2023-12-09 11:56:44 -0800

    Update singlevar.py

--- singlevar.py
+++ singlevar.py
@@ -1,11 +1,13 @@
 from sympy import symbols, Eq, solve, simplify
 
 def solve_single_variable_equation(equation_str):
-
     x = symbols('x')
 
+    # Use regex
+    equation_str = re.sub(r'(\d)([a-zA-Z])', r'\1*\2', equation_str)
+
     try:
-        equation = Eq(simplify(equation_str), 0)
+        equation = Eq(eval(equation_str), 0)
         solution = solve(equation, x)
 
         return solution
