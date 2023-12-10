b5e31e6a34466ff2a246297e9cd98605d0aae9d6
rodrjuan <147116629+rodrjuan@users.noreply.github.com>
2023-12-10 15:28:37 -0800

    Add files via upload

--- UnitConverterTester.py
+++ UnitConverterTester.py
@@ -4,14 +4,24 @@ def main():
     converter = UnitConverter.UnitConverter()
 
     # test metric length converter
-    print(converter.convertUnits("2 m p", "1 m"))
-    print(converter.convertUnits("40 m", "1 m k"))
-    print(converter.convertUnits("4 m k", "1 m mu"))
+    val = "1"
+    for c1 in UnitConverter.PREFIXES:
+        for c2 in UnitConverter.PREFIXES:
+            curr = val + " m " + c1
+            print(converter.convertLength(curr, "1 m " + c2))
+    
+    # test imperial lengths
+    val = "1"
+    for c1 in UnitConverter.IM_LENGTHS:
+        for c2 in UnitConverter.IM_LENGTHS:
+            curr = val + " " + c1
+            print(converter.convertLength(curr, "1.0 " + c2))
+
 
     # test temperature converter
-    # print(converter.convertTemps("2 F", "C"))
-    # print(converter.convertTemps("5 F", "K"))
-    # print(converter.convertTemps("200 K", "F"))
+    # print(converter.__convertTemps("2 F", "C"))
+    # print(converter.__convertTemps("5 F", "K"))
+    # print(converter.__convertTemps("200 K", "F"))
     return None
 
 if __name__ == "__main__":
